72262ab18e8d897a5d1c6f0e8148338df5a2b7ac
Witicer <770201276@qq.com>
2020-12-24 07:16:38 +0000

    图片验证码

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\UserRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
--- app/Http/Requests/Api/UserRequest.php
+++ app/Http/Requests/Api/UserRequest.php
@@ -1,9 +1,6 @@
 <?php
 
 namespace App\Http\Requests\Api;
-
-use Illuminate\Foundation\Http\FormRequest;
-
 class UserRequest extends FormRequest
 {
     /**
