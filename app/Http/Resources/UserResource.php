3d7df71f49750baa6662ab134c36de229a98263a
Witicer <770201276@qq.com>
2020-12-25 08:00:42 +0000

    修复 redis 和 predis 冲突的问题

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -7,16 +7,11 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class UserResource extends JsonResource
 {
     protected $showSensitiveFields = false;
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
+
     public function toArray($request)
     {
         if (!$this->showSensitiveFields) {
-            $this->resource->makeHidden(['phone', 'email']);
+            $this->resource->addHidden(['phone', 'email']);
         }
 
         $data = parent::toArray($request);
