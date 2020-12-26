a1e10cc835f97567e587ce056f36df7725debfbd
Witicer <770201276@qq.com>
2020-12-26 07:45:37 +0000

    话题回复

--- routes/api.php
+++ routes/api.php
@@ -67,6 +67,9 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
 
             // 修改、发布、删除帖子
             Route::resource('topics', 'TopicsController')->only(['store', 'update', 'destroy']);
+
+            // 发布回复
+            Route::post('topics/{topic}/replies', 'RepliesController@store')->name('topics.replies.store');
         });
     });
 });
