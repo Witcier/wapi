c12d16a17153016b5b65ab1129f3beb90590015b
Witicer <770201276@qq.com>
2020-12-28 06:21:06 +0000

    话题的回复列表和用户的回复列表

--- routes/api.php
+++ routes/api.php
@@ -57,6 +57,9 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
         // 帖子回复列表
         Route::get('topics/{topic}/replies', 'RepliesController@index')->name('topics.replies.index');
 
+        // 某个用户的评论列表
+        Route::get('users/{user}/replies', 'RepliesController@userIndex')->name('users.replies.index');
+
         //登录之后可以访问的接口
         Route::middleware('auth:api')->group(function () {
             // 用户的登录信息
