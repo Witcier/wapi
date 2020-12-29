2964f17d6e5c7852634b4a53c5def9f83b5d5894
Witicer <770201276@qq.com>
2020-12-29 03:55:31 +0000

    本地化

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
+Route::prefix('v1')->namespace('Api')->middleware('change-locale')->name('api.v1.')->group(function() {
     Route::middleware('throttle:'.config('api.rate_limits.sign'))->group(function () {
         // 图片验证码
         Route::post('captchas', 'CaptchasController@store')->name('captchas.store');
@@ -62,6 +62,9 @@ Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
 
         // 资源推荐
         Route::get('links', 'LinksController@index')->name('links.index');
+
+        // 活跃用户
+        Route::get('actived/users', 'UsersController@activedIndex')->name('actived.users.index');
 
         //登录之后可以访问的接口
         Route::middleware('auth:api')->group(function () {
